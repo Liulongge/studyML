10cfb88ca728305c2de182c726cf27e6c76b4ecf
RungeLiu <792286891@qq.com>
2024-04-02 01:17:32 +0800

    NEW: add 空洞卷积

--- 06-算子/02-卷积/05-空洞卷积.py
+++ 06-算子/02-卷积/05-空洞卷积.py
@@ -1,3 +1,5 @@
+# https://blog.csdn.net/weixin_44503976/article/details/127754679
+
 import numpy as np
 
 image = np.array([[[[1, 2, 3],
@@ -24,23 +26,49 @@ kernel = np.array([[[[1, 2, 3],
                    [1, 2, 3],
                    [1, 2, 3]]]])
 
+
+
+import numpy as np
+
+def convolve(image, kernel, padding=1, stride=1, dilation=2):
+
+    batch, channels, height, width = image.shape
+    kernel_n, kernel_c, kernel_h, kernel_w = kernel.shape
+
+    kernel_size = kernel_w
+    print(image.shape)
+    padded_image = np.pad(image, ((0, 0), (0, 0), (padding, padding), (padding, padding)), mode='constant', constant_values=0)
+    print(padded_image.shape)
+    # 空洞卷积输出shape计算公式
+    output_height = (height + 2 * padding - kernel_size - (kernel_size - 1) * (dilation -1 )) // stride + 1
+    output_width = (width + 2 * padding - kernel_size - (kernel_size - 1) * (dilation -1 )) // stride + 1
+    output_channel = kernel_n
+
+    output = np.zeros((1, output_channel, output_height, output_width))
+    print(output.shape)
+    for h in range(output_height):
+        for w in range(output_width):
+            for c in range(output_channel):
+                # 获取图像信息, n~n+3行, n~n+3列, 全部channel
+                window = padded_image[:, :,
+                                     h * stride:(h * stride + kernel_size * 2 - 1):2,
+                                     w * stride:(w * stride + kernel_size * 2 - 1):2]
+                # 图像与第x个kernel计算
+                output[0, :, h, w] = np.sum(window * kernel[0, :, :, :])
+
+    return output
+
+output = convolve(image, kernel, padding=2, stride=1, dilation=2)
+print("numpy, 空洞卷积:\n", output)
+
 import torch  
 import torch.nn as nn  
 from torch import tensor
 
-# 假设输入是一个四维张量，大小为(batch_size, channels, height, width)  
-# 例如: (1, 3, 32, 32) 表示一个批次的RGB图像，每张图像的大小为32x32  
 input_tensor = torch.from_numpy(image).float()
-  
-# 创建一个空洞卷积层  
-# 参数:  
-#   in_channels: 输入通道数  
-#   out_channels: 输出通道数  
-#   kernel_size: 卷积核的大小  
-#   dilation: 空洞卷积的扩张率  
+
 atrous_conv = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3, dilation=2, padding=2)  
 atrous_conv.weight.data = torch.from_numpy(kernel).float()
 # 将输入张量通过空洞卷积层  
 output_tensor = atrous_conv(input_tensor)  
-print("权重: ", atrous_conv.weight.data)
-print(output_tensor)  # 输出应为 (1, 16, 32, 32)，因为空洞卷积不会改变输入的空间尺寸（当padding正确时）
+print("pytorch, 空洞卷积:\n", output_tensor)  # 输出应为 (1, 16, 32, 32)，因为空洞卷积不会改变输入的空间尺寸（当padding正确时）
